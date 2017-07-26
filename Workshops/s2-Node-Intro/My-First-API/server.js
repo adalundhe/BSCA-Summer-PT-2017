a8a77e666b39e99bd5467d8c42ac4f71513c1963
Sean <sean.corbett@umontana.edu>
2017-07-25 20:47:57 -0600

    New PUT route pre-test

--- Workshops/s2-Node-Intro/My-First-API/server.js
+++ Workshops/s2-Node-Intro/My-First-API/server.js
@@ -33,6 +33,18 @@ app.post('/api', (req,res) => {
 })
 
 
+// UPDATE EXISTING CUSTOMER
+app.put('/api/:customer',(req,res) => {
+  let searchName = data.indexOf(req.params.customer)
+
+  if(searchName !== -1){
+    data[searchName] = req.body.newCustomer
+    res.json({ message: `Successfully modified customer: ${req.params.customer}`, data: data[searchName] })
+  }
+  else{
+    res.json({ message: `Couldn't find customer: ${req.params.customer}`, data: null })
+  }
+})
 
 
 app.listen(port,() => console.log(`Listening on port: ${port}`))
