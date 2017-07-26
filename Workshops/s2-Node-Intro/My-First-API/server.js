2dfb091c46974479fc19557fe00624df1f5cd3d9
Sean <sean.corbett@umontana.edu>
2017-07-25 19:50:23 -0600

    GET endpoint complete

--- Workshops/s2-Node-Intro/My-First-API/server.js
+++ Workshops/s2-Node-Intro/My-First-API/server.js
@@ -4,4 +4,10 @@ const app = express()
 let port = 3000
 let data = []
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// GET ALL
+app.get('/api',(req,res) => res.json({ message: "Pulling all customer data.", data: data }))
+
 app.listen(port,() => console.log(`Listening on port: ${port}`))
